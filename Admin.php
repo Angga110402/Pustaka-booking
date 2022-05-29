db4f6df2df1f025d6c97116c074feb54616e4872
Angga110402 <101782288+Angga110402@users.noreply.github.com>
2022-05-29 14:25:36 +0700

    Add files via upload

--- Admin.php
+++ Admin.php
@@ -2,7 +2,7 @@
  class Admin extends CI_Controller
 {     public function __construct()
     {         parent::__construct();         cek_login();
-	}      public function index()
+    }      public function index()
     {
 		$data['judul'] = 'Dashboard';
 		$data['user'] = $this->ModelUser->cekData(['email' => $this-
